8d44d24fe26bba1ca53c438ee880356509920371
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2024-10-29 21:13:58 -0400

    fetch data, display loading ind, and handle errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu"
+import Error from "./ui/Error"
+import Menu, {loader as menuLoader} from "./features/menu/Menu"
 import Cart from "./features/cart/Cart"
 import CreateOrder from "./features/order/CreateOrder"
 import Order from './features/order/CreateOrder';
@@ -10,6 +11,7 @@ import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: '/',
@@ -18,6 +20,8 @@ const router = createBrowserRouter([
       {
         path: '/menu',
         element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />, //Not necessary
       },
       {
         path: '/cart',

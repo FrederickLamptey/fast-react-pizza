7061751857938b4d9b1ef6f328825b0b6a0c35c5
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2024-11-01 22:01:05 -0400

    style buttons and edit prettier config file

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,31 +1,31 @@
-import { useState } from 'react';
-import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
-import { createOrder } from '../../services/apiRestaurant';
+import { useState } from "react";
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: 'Mediterranean',
+    name: "Mediterranean",
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: 'Vegetale',
+    name: "Vegetale",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: 'Spinach and Mushroom',
+    name: "Spinach and Mushroom",
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -34,7 +34,7 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation(); //This hook has different types of states such as idle, loading, submitting, etc
-  const isSubmitting = navigation.state === 'submitting';
+  const isSubmitting = navigation.state === "submitting";
   const formErrors = useActionData(); //This used to gain access to the data returned from the action function created below.
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
@@ -53,7 +53,7 @@ function CreateOrder() {
           <label>Phone number</label>
           <div>
             <input type="tel" name="phone" required />
-          </div> 
+          </div>
           {formErrors?.phone && <p> {formErrors.phone} </p>}
         </div>
 
@@ -78,8 +78,11 @@ function CreateOrder() {
         <div>
           {/*To include a field (cart) in the form with creating a new field in the form */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
-            {isSubmitting ? 'Placing Order...' : 'Order now'}
+          <button
+            disabled={isSubmitting}
+            className="bg-yellow-400 uppercase tracking-wide font-semibold text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? "Placing Order..." : "Order now"}
           </button>
         </div>
       </Form>
@@ -96,7 +99,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart), //converts cart's data from string to an object
-    priority: data.priority === 'on',
+    priority: data.priority === "on",
   };
 
   // console.log(order);
@@ -104,13 +107,14 @@ export async function action({ request }) {
 
   if (!isValidPhone(order.phone))
     errors.phone =
-      'Please give us your correct phone number. We might need it to contact you.';
+      "Please give us your correct phone number. We might need it to contact you.";
 
   if (Object.keys(errors).length > 0) return errors;
 
-  const newOrder = await createOrder(order);
+  // const newOrder = await createOrder(order);
 
-  return redirect(`/order/${newOrder.id}`);
+  // return redirect(`/order/${newOrder.id}`);
+  return null;
 }
 
 export default CreateOrder;
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,14 +1,14 @@
-import CreateUser from '../features/user/CreateUser'
+import CreateUser from "../features/user/CreateUser";
 
 function Home() {
   return (
-    <div className='my-10 sm:my-16 text-center px-4 '>
+    <div className="my-10 sm:my-16 text-center px-4 ">
       <h1 className="text-xl font-semibold text-center mb-8 md:text-3xl">
         The best pizza.
         <br />
         <span className="text-yellow-500">
           Straight out of the oven, straight to you.
-          </span>
+        </span>
       </h1>
       <CreateUser />
     </div>

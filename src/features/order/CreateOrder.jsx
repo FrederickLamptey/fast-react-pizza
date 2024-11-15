95d7c2a587f7865067e9a190f7a68d32567fbc72
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2024-11-15 17:01:46 -0500

    integrate geolocation with order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -40,7 +40,8 @@ const fakeCart = [
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
-  const username = useSelector(state => state.user.username);
+  const { username, status: addressStatus, position, address, error: errorAddress } = useSelector(state => state.user); 
+  const isLoadingAddress = addressStatus === 'loading';
 
   const navigation = useNavigation(); //This hook has different types of states such as idle, loading, submitting, etc
   const isSubmitting = navigation.state === "submitting";
@@ -56,7 +57,7 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-      <button onClick={()=> dispatch(fetchAddress())}>Get position</button>
+      
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
@@ -71,11 +72,15 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center relative">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
-            <input type="text" name="address" required className="input w-full"/>
+            <input type="text" name="address" disabled={isLoadingAddress} defaultValue={address} required className="input w-full" />
+             {addressStatus === 'error' && <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700"> {errorAddress} </p>}
           </div>
+          {!position.latitude && !position.longitude && <span className="absolute right-[3px] z-50 top-[3px] md:right-[5px] md:top-[5px]">
+            <Button disabled={isLoadingAddress} type="small" onClick={(e) => { e.preventDefault(); dispatch(fetchAddress()); }}>Get position</Button>
+          </span>}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -93,8 +98,9 @@ function CreateOrder() {
         <div>
           {/*To include a field (cart) in the form with creating a new field in the form */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+          <input type="hidden" name="position" value={position.longitude && position.latitude ? `${position.latitude}, ${position.longitude}`: ''} />
           <Button
-            disabled={isSubmitting}
+            disabled={isSubmitting || isLoadingAddress}
             type="primary"
             >
             {isSubmitting ? "Placing Order..." : `Order now for ${formatCurrency(totalPrice)}`}
@@ -116,6 +122,7 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart), //converts cart's data from string to an object
     priority: data.priority === "true",
   };
+  console.log(order)
 
   // console.log(order);
   const errors = {};
@@ -126,12 +133,13 @@ export async function action({ request }) {
 
   if (Object.keys(errors).length > 0) return errors;
 
+  //If everything is okay, create new order and redirect
   const newOrder = await createOrder(order);
  //Do Not overuse
   store.dispatch(clearCart())
 
   return redirect(`/order/${newOrder.id}`);
-  return null;
+
 }
 
 export default CreateOrder;
